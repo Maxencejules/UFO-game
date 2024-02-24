2ece5422c9343140c2b29d597e74991853381662
Maxence Jules <142577996+Maxencejules@users.noreply.github.com>
2024-02-23 20:18:15 -0500

    Create UFO_functions.hpp

---
+++ UFO_functions.hpp
@@ -0,0 +1,12 @@
+#ifndef UFO_FUNCTIONS_HPP
+#define UFO_FUNCTIONS_HPP
+
+#include <vector>
+#include <string>
+
+void greet();
+void display_misses(int misses);
+void display_status(std::string answer, std::string codeword);
+void end_game(std::string answer, std::string codeword);
+
+#endif
